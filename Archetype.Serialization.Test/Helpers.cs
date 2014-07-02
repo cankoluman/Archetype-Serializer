2769d13d3ecaef453d9155108ae9b1869b4a257b
cankoluman <n2dbrx@gmail.com>
2014-07-02 16:53:30 +0100

    Start on set-up of tests - TO DO: path resolution

---
+++ Archetype.Serialization.Test/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using _7._1._4.ConsoleApp;
+
+namespace Archetype.Serialization.Test
+{
+    public class Helpers
+    {
+        public Commands ConsoleCommands { get; private set; }
+
+        public Helpers()
+        {
+            ConsoleCommands = new Commands();
+        }
+
+    }
+}
---
+++ Archetype.Serialization.Test/SerializationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Archetype.Serialization.Test
+{
+    [TestFixture]
+    public class SerializationTests
+    {
+        private Helpers _testHelpers;
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            _testHelpers = new Helpers();   
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            _testHelpers.ConsoleCommands.Exit();
+        }
+
+        [TestCase("simpleModel")]
+        public void PropertyIsNotNullOrEmpty(string propAlias)
+        {
+            Assert.IsNotNullOrEmpty(_testHelpers.ConsoleCommands.GetArchetypeJsonFor(propAlias));
+        }
+    }
+}
