70cb8e3c3db2525992f1cb09aedc8c5b1d598fa8
cankoluman <n2dbrx@gmail.com>
2014-07-03 10:21:05 +0100

    Added some basic validation tests

--- Archetype.Serialization.Test/SerializationTests.cs
+++ Archetype.Serialization.Test/SerializationTests.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Archetype.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
 using NUnit.Framework;
 
 namespace Archetype.Serialization.Test
@@ -28,6 +31,30 @@ namespace Archetype.Serialization.Test
         public void PropertyIsNotNullOrEmpty(string propAlias)
         {
             Assert.IsNotNullOrEmpty(_testHelpers.ConsoleCommands.GetArchetypeJsonFor(propAlias));
+        }
+
+        [TestCase("simpleModel")]
+        public void IsJsonValid(string propAlias)
+        {
+            var json = _testHelpers.ConsoleCommands.GetArchetypeJsonFor(propAlias);
+            Assert.DoesNotThrow(() => JsonConvert.DeserializeObject(json));
+        }
+
+        [TestCase("simpleModel")]
+        public void IsJsonValidArchetype(string propAlias)
+        {
+            var json = _testHelpers.ConsoleCommands.GetArchetypeJsonFor(propAlias);
+            Assert.DoesNotThrow(() => JsonConvert.DeserializeObject<ArchetypeModel>(json));
+        }
+
+        [TestCase("simpleModel")]
+        public void JsonDeserializesToArchetype(string propAlias)
+        {
+            var json = _testHelpers.ConsoleCommands.GetArchetypeJsonFor(propAlias);
+            var model = JsonConvert.DeserializeObject<ArchetypeModel>(json);
+
+            Assert.IsInstanceOf<ArchetypeModel>(model);
+            Assert.IsNotNull(model);
         }
     }
 }
