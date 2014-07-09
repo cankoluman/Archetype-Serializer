fcf735fa2e9099b0a3fbe589f530e851121499ea
cankoluman <n2dbrx@gmail.com>
2014-07-09 14:57:05 +0100

    Update versions and generate nuspec file

--- Archetype.Serializer/Properties/AssemblyInfo.cs
+++ Archetype.Serializer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Archetype.Serializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple serializer to use with Archetype.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Can Koluman")]
 [assembly: AssemblyProduct("Archetype.Serializer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Can Koluman 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-alpha")]
+[assembly: AssemblyFileVersion("0.2.0-alpha")]
 
 [assembly: InternalsVisibleTo("Archetype.Serializer.Test")]
