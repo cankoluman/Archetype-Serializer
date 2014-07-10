3f19d68e7ad40c6dca1892bccb515ba53a86af4f
cankoluman <n2dbrx@gmail.com>
2014-07-10 16:00:41 +0100

    version bump 0.2.4-alpha

--- Archetype.Serializer/Properties/AssemblyInfo.cs
+++ Archetype.Serializer/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyInformationalVersion("0.2.3-alpha")]
-[assembly: AssemblyFileVersion("0.2.3-alpha")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyInformationalVersion("0.2.4-alpha")]
+[assembly: AssemblyFileVersion("0.2.4-alpha")]
 
 [assembly: InternalsVisibleTo("Archetype.Serializer.Test")]
