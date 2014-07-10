e0a170c42d44490c022a4b556cd32da3b331294f
cankoluman <n2dbrx@gmail.com>
2014-07-10 09:58:06 +0100

    Version to 0.2.2-alpha

--- Archetype.Serializer/Properties/AssemblyInfo.cs
+++ Archetype.Serializer/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyInformationalVersion("0.2.1-alpha")]
-[assembly: AssemblyFileVersion("0.2.1-alpha")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyInformationalVersion("0.2.2-alpha")]
+[assembly: AssemblyFileVersion("0.2.2-alpha")]
 
 [assembly: InternalsVisibleTo("Archetype.Serializer.Test")]
