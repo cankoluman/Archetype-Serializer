bb4997ed97d6349d234ef767c605281add97c931
cankoluman <n2dbrx@gmail.com>
2014-07-30 10:09:09 +0100

    Bump version to 0.6.1-beta, updated compatibility notice to include Archetype 1.4-beta.

--- Archetype.Serializer/Properties/AssemblyInfo.cs
+++ Archetype.Serializer/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyInformationalVersion("0.6.0-beta")]
-[assembly: AssemblyFileVersion("0.6.0-beta")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyInformationalVersion("0.6.1-beta")]
+[assembly: AssemblyFileVersion("0.6.1-beta")]
 
 [assembly: InternalsVisibleTo("Archetype.Serializer.Test")]
